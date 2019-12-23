82b9d00530e3f754606d9bd00a195374f4bb18fb
Sofian Ardiansyah Wahab <limadawai@gmail.com>
2019-12-23 08:57:12 +0700

    Update 23 dec 2019

--- src/main/java/com/jica/sdg/SdgApplication.java
+++ src/main/java/com/jica/sdg/SdgApplication.java
@@ -1,16 +1,18 @@
 package com.jica.sdg;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.ParentContextCloserApplicationListener;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SdgApplication {
-
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SdgApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SdgApplication.class, args);
