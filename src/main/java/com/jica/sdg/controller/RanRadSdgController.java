b5409236e40d8d79d43bbde67358c080d93a36e0
Oka Lingga Kencana <oka.kencan@gmail.com>
2019-12-27 13:48:32 +0700

    delete sdg goals

--- src/main/java/com/jica/sdg/controller/RanRadSdgController.java
+++ src/main/java/com/jica/sdg/controller/RanRadSdgController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,6 +71,12 @@ public class RanRadSdgController {
         hasil.put("content",list);
         return hasil;
     }
+	
+	@DeleteMapping("admin/delete-sdgGoals/{id}")
+	@ResponseBody
+	public void deleteSdg(@PathVariable("id") String id) {
+		sdgGoalsService.deleteSdgGoals(id);
+	}
 	
 	@GetMapping("admin/ran_rad/sdg/goals/{id}/target")
     public String target(Model model, @PathVariable("id") String id) {
