bc9d21f0d3087e1b49f36cf34c505438df6ea856
Oka Lingga Kencana <oka.kencan@gmail.com>
2020-01-14 08:50:49 +0700

    bug user

--- src/main/java/com/jica/sdg/repository/UserRepository.java
+++ src/main/java/com/jica/sdg/repository/UserRepository.java
@@ -14,10 +14,10 @@ public interface UserRepository extends CrudRepository<User, Integer> {
 
     List<User> findByUserName(String userName);
     
-    @Query(value = "select a.id_user, a.username, b.nm_role, a.detail,a.name from ref_user a left join ref_role b on a.id_role = b.id_role where b.id_prov = :id_prov",nativeQuery = true)
+    @Query(value = "select a.id_user, a.username, b.nm_role, a.detail,a.name,b.id_prov from ref_user a left join ref_role b on a.id_role = b.id_role where b.id_prov = :id_prov",nativeQuery = true)
 	public List findByProvince(@Param("id_prov") String id_prov); 
     
-    @Query(value = "select a.id_user, a.username, b.nm_role, a.detail,a.name from ref_user a left join ref_role b on a.id_role = b.id_role",nativeQuery = true)
+    @Query(value = "select a.id_user, a.username, b.nm_role, a.detail,a.name,b.id_prov from ref_user a left join ref_role b on a.id_role = b.id_role",nativeQuery = true)
 	public List findAllGrid(); 
 
 }
