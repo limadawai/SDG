7077fb0afb4f69f0909f5c0f704cfa76c7261b1f
Sofian Ardiansyah Wahab <limadawai@gmail.com>
2020-02-25 01:12:31 +0700

    Revisi Grafik

--- src/main/java/com/jica/sdg/controller/ReportController.java
+++ src/main/java/com/jica/sdg/controller/ReportController.java
@@ -287,33 +287,23 @@ public class ReportController {
     @GetMapping("admin/reportgovgrap")
     public @ResponseBody List<Object> reportgovgrap(@RequestParam("id_sdg_goals") int idsdg, @RequestParam("id_sdg_target") int idtarget,
     		@RequestParam("id_sdg_indicator") int idsdgindi, @RequestParam("id_gov_indicator") int id) {
-        String sql = "SELECT a.*, b.nm_unit FROM gov_map a LEFT JOIN " +
-                "ref_unit b ON b.id_unit = (SELECT unit FROM gov_indicator WHERE id = a.id_gov_indicator) "
-                + "WHERE id_goals = :id_goals AND id_target = :id_target AND id_indicator = :id_indicator AND id_gov_indicator = :id";
+        String sql = "SELECT a.*, b.nm_unit FROM gov_map a LEFT JOIN "
+                +"ref_unit b ON b.id_unit = (SELECT unit FROM gov_indicator WHERE id = a.id_gov_indicator) "
+                + "WHERE id_goals = :id_goals AND id_target = :id_target AND id_indicator = :id_indicator AND id_gov_indicator = :id_gov_indicator";
         Query query = manager.createNativeQuery(sql);
         query.setParameter("id_goals", idsdg);
         query.setParameter("id_target", idtarget);
         query.setParameter("id_indicator", idsdgindi);
-        query.setParameter("id", id);
+        query.setParameter("id_gov_indicator", id);
         List list = query.getResultList();
         return list;
     }
     
     @GetMapping("admin/targetgov")
     public @ResponseBody List<Object> targetgov(@RequestParam("id_gov_indicator") int idindi, @RequestParam("year") String tahun) {
-    	String sql = "SELECT value FROM gov_target WHERE id_gov_indicator = :id_gov_indicator AND year = :year";
+    	String sql = "SELECT value FROM gov_target WHERE id_gov_indicator = :id_gov_indicator AND year = :year ORDER BY YEAR ASC";
     	Query query = manager.createNativeQuery(sql);
     	query.setParameter("id_gov_indicator", idindi);
-    	query.setParameter("year", tahun);
-        List list = query.getResultList();
-        return list;
-    }
-    
-    @GetMapping("admin/targetnsa")
-    public @ResponseBody List<Object> targetnsa(@RequestParam("id_nsa_indicator") int idindi, @RequestParam("year") String tahun) {
-    	String sql = "SELECT value FROM nsa_target WHERE id_nsa_indicator = :id_nsa_indicator AND year = :year";
-    	Query query = manager.createNativeQuery(sql);
-    	query.setParameter("id_nsa_indicator", idindi);
     	query.setParameter("year", tahun);
         List list = query.getResultList();
         return list;
@@ -349,6 +339,51 @@ public class ReportController {
         return list;
     }
     
+    //****************************** NSA Grap *******************************
+//    @GetMapping("admin/reportnsagrap")
+//    public @ResponseBody List<Object> reportnsagrap(@RequestParam("id_nsa_indicator") int id) {
+//        String sql = "SELECT a.*, b.nm_unit, c.value AS val1, d.value AS val2, e.value AS val3, f.value AS val4, g.value AS val5, " +
+//                "h.funding_source, (i.achievement1+i.achievement2+i.achievement3+i.achievement4) FROM nsa_map a LEFT JOIN " +
+//                "ref_unit b ON b.id_unit = (SELECT unit FROM nsa_indicator WHERE id = a.id_nsa_indicator) LEFT JOIN " +
+//                "nsa_target c ON c.id_nsa_indicator = a.id_nsa_indicator AND c.year = (SELECT start_year FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
+//                "nsa_target d ON d.id_nsa_indicator = a.id_nsa_indicator AND d.year = (SELECT start_year+1 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
+//                "nsa_target e ON e.id_nsa_indicator = a.id_nsa_indicator AND e.year = (SELECT start_year+2 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
+//                "nsa_target f ON f.id_nsa_indicator = a.id_nsa_indicator AND f.year = (SELECT start_year+3 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
+//                "nsa_target g ON g.id_nsa_indicator = a.id_nsa_indicator AND g.year = (SELECT start_year+4 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
+//                "nsa_funding h ON h.id_nsa_indicator = a.id_nsa_indicator LEFT JOIN " +
+//                "entry_nsa_budget i ON i.id_monper = a.id_monper " +
+//                "WHERE a.id_nsa_indicator = :id";
+//        Query query = manager.createNativeQuery(sql);
+//        query.setParameter("id", id);
+//        List list = query.getResultList();
+//        return list;
+//    }
+    
+    @GetMapping("admin/reportnsagrap")
+    public @ResponseBody List<Object> reportnsagrap(@RequestParam("id_sdg_goals") int idsdg, @RequestParam("id_sdg_target") int idtarget,
+    		@RequestParam("id_sdg_indicator") int idsdgindi, @RequestParam("id_gov_indicator") int id) {
+        String sql = "SELECT a.*, b.nm_unit FROM nsa_map a LEFT JOIN "
+                +"ref_unit b ON b.id_unit = (SELECT unit FROM nsa_indicator WHERE id = a.id_nsa_indicator) "
+                + "WHERE id_goals = :id_goals AND id_target = :id_target AND id_indicator = :id_indicator AND id_nsa_indicator = :id_nsa_indicator";
+        Query query = manager.createNativeQuery(sql);
+        query.setParameter("id_goals", idsdg);
+        query.setParameter("id_target", idtarget);
+        query.setParameter("id_indicator", idsdgindi);
+        query.setParameter("id_nsa_indicator", id);
+        List list = query.getResultList();
+        return list;
+    }
+    
+    @GetMapping("admin/targetnsa")
+    public @ResponseBody List<Object> targetnsa(@RequestParam("id_nsa_indicator") int idindi, @RequestParam("year") String tahun) {
+    	String sql = "SELECT value FROM nsa_target WHERE id_nsa_indicator = :id_nsa_indicator AND year = :year ORDER BY YEAR ASC";
+    	Query query = manager.createNativeQuery(sql);
+    	query.setParameter("id_nsa_indicator", idindi);
+    	query.setParameter("year", tahun);
+        List list = query.getResultList();
+        return list;
+    }
+    
     @GetMapping("admin/realnsayear")
     public @ResponseBody List<Object> realnsayear(@RequestParam("id_monper") int idmonper, @RequestParam("year") String tahun) {
     	String sql = "SELECT achievement1 FROM entry_nsa_indicator WHERE year_entry = :year AND id_monper = :id_monper";
@@ -375,25 +410,6 @@ public class ReportController {
     	Query query = manager.createNativeQuery(sql);
     	query.setParameter("id_monper", idmonper);
     	query.setParameter("year", tahun);
-        List list = query.getResultList();
-        return list;
-    }
-
-    @GetMapping("admin/reportnsagrap")
-    public @ResponseBody List<Object> reportnsagrap(@RequestParam("id_nsa_indicator") int id) {
-        String sql = "SELECT a.*, b.nm_unit, c.value AS val1, d.value AS val2, e.value AS val3, f.value AS val4, g.value AS val5, " +
-                "h.funding_source, (i.achievement1+i.achievement2+i.achievement3+i.achievement4) FROM nsa_map a LEFT JOIN " +
-                "ref_unit b ON b.id_unit = (SELECT unit FROM nsa_indicator WHERE id = a.id_nsa_indicator) LEFT JOIN " +
-                "nsa_target c ON c.id_nsa_indicator = a.id_nsa_indicator AND c.year = (SELECT start_year FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
-                "nsa_target d ON d.id_nsa_indicator = a.id_nsa_indicator AND d.year = (SELECT start_year+1 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
-                "nsa_target e ON e.id_nsa_indicator = a.id_nsa_indicator AND e.year = (SELECT start_year+2 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
-                "nsa_target f ON f.id_nsa_indicator = a.id_nsa_indicator AND f.year = (SELECT start_year+3 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
-                "nsa_target g ON g.id_nsa_indicator = a.id_nsa_indicator AND g.year = (SELECT start_year+4 FROM ran_rad WHERE id_monper = a.id_monper) LEFT JOIN " +
-                "nsa_funding h ON h.id_nsa_indicator = a.id_nsa_indicator LEFT JOIN " +
-                "entry_nsa_budget i ON i.id_monper = a.id_monper " +
-                "WHERE a.id_nsa_indicator = :id";
-        Query query = manager.createNativeQuery(sql);
-        query.setParameter("id", id);
         List list = query.getResultList();
         return list;
     }
