94d4bde38568c54a84270d0a34777ca28472f64a
Oka Lingga Kencana <oka.kencan@gmail.com>
2020-04-19 23:30:15 +0700

    bug

--- src/main/java/com/jica/sdg/controller/DataEntryController.java
+++ src/main/java/com/jica/sdg/controller/DataEntryController.java
@@ -1025,6 +1025,26 @@ public class DataEntryController {
         return hasil;
     }
     
+    @GetMapping("admin/cek-show-report/{id_monper}/{year}/{type}")
+    public @ResponseBody Map<String, Object> cekShowReport(@PathVariable("id_monper") String id_monper, @PathVariable("year") String year, @PathVariable("type") String type) {
+    	List list;
+    	String sql  = " select "
+    			+ " (select count(id) from entry_show_report as a where a.id_monper = :id_monper and a.year = :year and a.type = :type and a.period = 1) as cek1, "
+    	+ " (select count(id) from entry_show_report as a where a.id_monper = :id_monper and a.year = :year and a.type = :type and a.period = 2) as cek2, "
+    	+ " (select count(id) from entry_show_report as a where a.id_monper = :id_monper and a.year = :year and a.type = :type and a.period = 3) as cek3, "
+    	+ " (select count(id) from entry_show_report as a where a.id_monper = :id_monper and a.year = :year and a.type = :type and a.period = 4) as cek4, "
+    	+ " (select count(id) from entry_show_report as a where a.id_monper = :id_monper and a.year = :year and a.type = :type and a.period = 0) as cek5 ";
+        Query query = em.createNativeQuery(sql);
+        query.setParameter("id_monper", id_monper);
+        query.setParameter("year", year);
+        query.setParameter("type", type);
+
+        list   = query.getResultList();
+        Map<String, Object> hasil = new HashMap<>();
+        hasil.put("cek",list);
+        return hasil;
+    }
+    
     @PostMapping(path = "admin/save-entry-gov_prog_indicator/{achiev}", consumes = "application/json", produces = "application/json")
     @ResponseBody
     @Transactional
